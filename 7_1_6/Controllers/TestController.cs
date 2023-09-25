697ab7a9f0989390eb6d27c31b6c7618c9691e82
Paul <paul.yestchick@gmail.com>
2023-09-25 19:29:59 +0300

    adding dbcontext from _7_1_2

--- 7_1_6/Controllers/TestController.cs
+++ 7_1_6/Controllers/TestController.cs
@@ -1,17 +1,24 @@
-﻿using _7_1_6.Entities;
+﻿using _7_1.Entities;
+using _7_1_2;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _7_1_6.Controllers
 {
+
+
   [ApiController]
   [Route("[controller]")]
   public class TestController : ControllerBase
   {
+    private AppDbContext_7_1_2 _dbContext = new AppDbContext_7_1_2();
+
     [HttpGet]
-    public IEnumerable<int> Get()
+    public IEnumerable<RefContractor> Get()
     {
-      return new[] { 1, 2, 3 };
+      return _dbContext.Contractor.ToList();
     }
+
+
   }
 }
