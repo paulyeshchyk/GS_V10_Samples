2a24f992e99b2a53f29ff8c1815b5b6950627eb3
Paul <paul.yestchick@gmail.com>
2023-09-25 21:48:05 +0300

    some improvements

---
+++ 7_1_6/API/Controllers/ContractorController.cs
@@ -0,0 +1,54 @@
+﻿using _7_1.Entities;
+using _7_1_2;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _7_1_6.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")] //конечный путь https://localhost:7175/api/Contractor
+  public class ContractorController : ControllerBase
+  {
+    private AppDbContext_7_1_2 _dbContext = new AppDbContext_7_1_2();
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<RefContractor>>> GetContractorList()
+    {
+      return await Task.FromResult(_dbContext.Contractor.ToList());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<RefContractor>> GetContractor(int id)
+    {
+      var result = await _dbContext.Contractor.FindAsync(id);
+      if (result == null)
+      {
+        return NotFound();
+      }
+      return result;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<int>> DeleteContractor(int id)
+    {
+      var contractor = await _dbContext.Contractor.FindAsync(id);
+      if (contractor == null)
+      {
+        return NotFound();
+      }
+      _dbContext.Contractor.Remove(contractor);
+      await _dbContext.SaveChangesAsync();
+      return 0;
+    }
+
+
+    [HttpPost]
+    public async Task<ActionResult<RefContractor>> PostContractor(RefContractor contractor)
+    {
+      _dbContext.Contractor.Add(contractor);
+      await _dbContext.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetContractor), new { Id = contractor.Id, Name = contractor.Name }, contractor);
+    }
+
+
+  }
+}
